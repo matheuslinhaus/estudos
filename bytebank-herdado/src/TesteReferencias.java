e83a99ecdff2a5d80aa3a81902fdbb4600ea500a
Matheus Linhaus <matheuslinhauss@hotmail.com>
2021-11-10 15:38:09 -0300

    alteracoes ded aula

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -7,15 +7,12 @@ public class TesteReferencias {
 		g1.setNome("Matheus");
 		g1.setSalario(5000.00);
 		
-		Funcionario f1 = new Funcionario();
-		f1.setSalario(2000.00);
 		
 		EditorVideo ev = new EditorVideo();
 		ev.setSalario(2500.00);
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
-		controle.registra(f1);
 		controle.registra(ev);
 		
 		System.out.println(controle.getSoma());
